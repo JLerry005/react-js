bcab9bef4aec1f11c42a239ed9741acaea18cfa0
JLerry05 <jlerry005@gmail.com>
2023-01-26 23:03:11 +0800

    01-26-23

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './img/logo192.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <LearnReact />
+        </div>
+    );
 }
+
+const Header = () => {
+
+    return (
+        <header>
+            <nav>
+                <img src={logo} className="" alt='' width="40px" />
+            </nav>
+        </header>
+    )
+}
+
+const MainContent = () => {
+    return (
+        <main>
+            <section>
+                <h1>Reason I'm learning React</h1>
+                <ol>
+                    <li>I want to learn a framework of JavaScript</li>
+                    <li>React is fun to learn</li>
+                    <li>I love to learn a new things</li>
+                    <li>I want to build an interactive Website just like:
+                        <ul>
+                            <li>ChatApp</li>
+                            <li>Movies Website</li>
+                            <li>Forum Website</li>
+                        </ul>
+                    </li>
+                    <li>I love to learn a new things</li>
+                </ol>
+            </section>
+        </main>
+    )
+}
+
+const Footer = () => {
+    return (
+        <footer>
+            <small>© 2023 Hapatinga development. All right reserved.</small>
+        </footer>
+    )
+}
+
+const LearnReact = () => {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
 
 export default App;
